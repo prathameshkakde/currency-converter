2df53e03bf85080db1e168600fdbf58f1311021d
Prathamesh Kakde <kakdeprathamesh860@gmail.com>
2026-04-01 22:45:06 +0530

    Integrate real-time currency conversion using external API

--- src/main/java/com/currencyconverter/currency_converter/HelloController.java
+++ src/main/java/com/currencyconverter/currency_converter/HelloController.java
@@ -3,6 +3,7 @@ package com.currencyconverter.currency_converter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +21,23 @@ public class HelloController {
             @RequestParam String to,
             @RequestParam double amount){
 
-        double conversionRate = 80; //dummy rate (USD to INR)
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Relaced API Key from ExchangeRate-API - https://v6.exchangerate-api.com/v6/ca891384bb40e6a172a97792/latest/USD
+        String apiKey = "ca891384bb40e6a172a97792";
+
+        // USD as base for simplicity first
+        String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + from;
+
+        // Call external API and get response as Map
+        Map responseFromApi = restTemplate.getForObject(url, Map.class);
+
+        // Extract rates
+        Map<String, Double> rates = (Map<String, Double>) responseFromApi.get("conversion_rates");
+
+        // Get conversion rate for target currency
+        double conversionRate = rates.get(to);
+
         double convertedAmount = amount * conversionRate;
 
         Map<String, Object> response = new HashMap<>();
