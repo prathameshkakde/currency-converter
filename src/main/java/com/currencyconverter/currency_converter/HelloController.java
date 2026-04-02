4947013c1e04a4a3a4caf10913bf4bca62c6c420
Prathamesh Kakde <kakdeprathamesh860@gmail.com>
2026-04-02 23:57:16 +0530

    refactor: move currency conversion logic to service layer

--- src/main/java/com/currencyconverter/currency_converter/HelloController.java
+++ src/main/java/com/currencyconverter/currency_converter/HelloController.java
@@ -1,51 +1,28 @@
 package com.currencyconverter.currency_converter;
 
+import com.currencyconverter.currency_converter.service.CurrencyService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import java.util.HashMap;
+
 import java.util.Map;
 
-@RestController //This tells Spring this class handles API requests
+@RestController // Mark this class as API controller
 public class HelloController {
 
-    @GetMapping("/hello") //When user goes to /hello
-    public String sayHello(){
-        return "Hello, Currency Converter is running!";
+    private final CurrencyService currencyService;
+
+    // Constructor (Sprint uses this to inject service)
+    public HelloController(CurrencyService currencyService) {
+        this.currencyService = currencyService;
     }
 
     @GetMapping("/convert")
     public Map<String, Object> convertCurrency(
             @RequestParam String from,
             @RequestParam String to,
-            @RequestParam double amount){
+            @RequestParam double amount) {
 
-        RestTemplate restTemplate = new RestTemplate();
-
-        // Relaced API Key from ExchangeRate-API - https://v6.exchangerate-api.com/v6/ca891384bb40e6a172a97792/latest/USD
-        String apiKey = "ca891384bb40e6a172a97792";
-
-        // USD as base for simplicity first
-        String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + from;
-
-        // Call external API and get response as Map
-        Map responseFromApi = restTemplate.getForObject(url, Map.class);
-
-        // Extract rates
-        Map<String, Double> rates = (Map<String, Double>) responseFromApi.get("conversion_rates");
-
-        // Get conversion rate for target currency
-        double conversionRate = rates.get(to);
-
-        double convertedAmount = amount * conversionRate;
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("from", from);
-        response.put("to", to);
-        response.put("amount", amount);
-        response.put("convertedAmount", convertedAmount);
-
-        return response;
+        return currencyService.convert(from, to, amount);
     }
 }
